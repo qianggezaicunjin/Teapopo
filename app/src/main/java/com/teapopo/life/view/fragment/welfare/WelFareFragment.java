29672fd9b05b2292662ff4a5eacf7c056ed8ba52
qianggezaicunjin <498133565@qq.com>
2016-06-28 09:36:51 +0800

    优化了UI界面

--- app/src/main/java/com/teapopo/life/view/fragment/welfare/WelFareFragment.java
+++ app/src/main/java/com/teapopo/life/view/fragment/welfare/WelFareFragment.java
@@ -1,32 +1,21 @@
 package com.teapopo.life.view.fragment.welfare;
 
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.widget.TextView;
-import com.teapopo.life.R;
+
 import com.teapopo.life.databinding.FragmentWelfareBinding;
-import com.teapopo.life.model.articleinfo.ArticleInfoModel;
 
 import com.teapopo.life.view.activity.MainActivity;
 import com.teapopo.life.view.adapter.recyclerview.EventListAdapter;
-import com.teapopo.life.view.adapter.viewpager.WelfareAdapter;
-import com.teapopo.life.view.customView.HtmlTextView.HtmlTextView;
-import com.teapopo.life.view.customView.RequestView;
 import com.teapopo.life.view.fragment.BaseFragment;
 import com.teapopo.life.viewModel.welfare.EventListViewModel;
 
 
 import javax.inject.Inject;
-
-import timber.log.Timber;
 
 /**
  * Created by louiszgm-pc on 2016/5/20.
